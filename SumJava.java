dd89dceae7f4d2fb97a136d2328029411b5f41e7
Omkar Tukaram Aghav <omkar.t.aghav@gmail.com>
2023-12-31 19:16:18 +0530

    initial commit

---
+++ SumJava.java
@@ -0,0 +1,8 @@
+public class SumJava {
+     public static void main(String args[]) {
+        int a=5;
+        int b=6;
+        int add=a+b;
+         System.out.println("Addition of Two Numbers is: "+add);
+    }
+}
